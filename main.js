4ab95eee0aac4722cd9796b6759aa39886f76e84
Arsalaann <arsalankhanxk@gmail.com>
2023-06-19 00:16:30 +0530

    Create contact

---
+++ main.js
@@ -0,0 +1,13 @@
+class Contact{
+    constructor(firstName,lastName,address,city,state,zip,phoneNumber,EmailId){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.phoneNumber=phoneNumber;
+        this.EmailId=EmailId;
+    }
+}
+let contact1=new Contact("arsalan","khan","abc","bhopal","mp",462038,290923023,"abc@123.com");
